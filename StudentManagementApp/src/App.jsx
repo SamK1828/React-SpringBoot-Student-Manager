de59404c0f0519d45e8aebf69207631b87fd70f9
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-10-20 07:06:42 +0530

    Refactor App component layout and add route for editing student details

--- StudentManagementApp/src/App.jsx
+++ StudentManagementApp/src/App.jsx
@@ -4,21 +4,34 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Students from "./pages/Students";
 import AddStudent from "./pages/AddStudent";
+import EditStudent from "./pages/EditStudent";
 import About from "./pages/About";
 import Footer from "./components/Footer";
-
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/students" element={<Students />} />
-        <Route path="/students/new" element={<AddStudent />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer />
+      <div className="bg-gray-50 min-h-screen">
+        {/* Navbar Always Visible */}
+        <Navbar />
+
+        {/* Main Content */}
+        <div className="p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/add-student" element={<AddStudent />} />
+            <Route path="/edit-student/:id" element={<EditStudent />} />
+            <Route path="*" element={<h2 className="text-center text-2xl mt-10">404 - Page Not Found</h2>} />
+            
+          </Routes>
+          
+        </div>
+        <Footer/>
+      </div>
+      
     </Router>
+    
   );
 };
 
