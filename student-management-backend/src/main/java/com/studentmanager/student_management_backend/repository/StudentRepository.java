481388c8f2302d140ee4db43b87b3dc1387463e5
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-10-24 17:48:30 +0530

    Added Find Name Functionality with Name Paramater

--- student-management-backend/src/main/java/com/studentmanager/student_management_backend/repository/StudentRepository.java
+++ student-management-backend/src/main/java/com/studentmanager/student_management_backend/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.studentmanager.student_management_backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.studentmanager.student_management_backend.entity.Student;
 
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByNameContainingIgnoreCase(String name);
 }
