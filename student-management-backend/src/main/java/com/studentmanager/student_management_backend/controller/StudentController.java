080d344d67dd7d2855fc1c2f31f39c8014b3787d
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-10-24 17:47:55 +0530

    Added Search Student API to Controller

--- student-management-backend/src/main/java/com/studentmanager/student_management_backend/controller/StudentController.java
+++ student-management-backend/src/main/java/com/studentmanager/student_management_backend/controller/StudentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studentmanager.student_management_backend.entity.Student;
@@ -59,5 +60,10 @@ public class StudentController {
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     service.delete(id);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/search")
+  public List<Student> searchStudents(@RequestParam String name) {
+    return service.searchByName(name);
   }
 }
