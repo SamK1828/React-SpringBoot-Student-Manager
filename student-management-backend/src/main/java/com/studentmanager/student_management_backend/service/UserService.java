a5b7ad67d49ef4a850579d8db445be915dacf843
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-11-06 16:14:06 +0530

    Added User Service Logic to Login and Register

---
+++ student-management-backend/src/main/java/com/studentmanager/student_management_backend/service/UserService.java
@@ -0,0 +1,27 @@
+package com.studentmanager.student_management_backend.service;
+
+
+
+import com.studentmanager.student_management_backend.entity.User;
+import com.studentmanager.student_management_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(String email, String password) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new RuntimeException("Email already registered!");
+        }
+        User user = new User(email, passwordEncoder.encode(password));
+        return userRepository.save(user);
+    }
+}
