df86da1a201af6931d6500144e35291086b6376e
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-11-12 20:13:34 +0530

    Created Admin Controller

---
+++ student-management-backend/src/main/java/com/studentmanager/student_management_backend/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.studentmanager.student_management_backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @GetMapping("/dashboard")
+    public String adminDashboard() {
+        return "Welcome Admin! This is your dashboard.";
+    }
+}
