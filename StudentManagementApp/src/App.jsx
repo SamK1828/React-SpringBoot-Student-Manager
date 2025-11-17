817ae63649162f80b35b65b8bafdd3bc10a170d9
Samarth Deelip Kalegaonkar <kalegaonkarsamarth@gmail.com>
2025-11-17 22:28:14 +0530

    Refactor App.jsx

--- StudentManagementApp/src/App.jsx
+++ StudentManagementApp/src/App.jsx
@@ -21,20 +21,20 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/students" element={<Students />} />
-            <Route path="/about" element={<About/>} />
+            <Route path="/about" element={<About />} />
             <Route path="/add-student" element={<AddStudent />} />
             <Route path="/edit-student/:id" element={<EditStudent />} />
             <Route path="*" element={<h2 className="text-center text-2xl mt-10">404 - Page Not Found</h2>} />
             <Route path="/import-export" element={<ImportExport />} />
           </Routes>
-          
+
         </div>
         <ToastContainer position="top-center" autoClose={3000} />
-        <Footer/>
+        <Footer />
       </div>
-      
+
     </Router>
-    
+
   );
 };
 
